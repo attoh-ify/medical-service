e56a653056077f940de2466f446822e8c76e7833
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2025-12-09 10:44:36 +0100

    feat: initial project creation

---
+++ medical-service/src/main/java/org/health/medical_service/MedicalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.health.medical_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServiceApplication.class, args);
+	}
+
+}

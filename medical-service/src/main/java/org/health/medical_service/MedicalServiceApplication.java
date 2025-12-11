ea700bfeba549b3337e054bc0e7eb4d306b98d48
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2025-12-11 15:18:09 +0100

    feat: AOP
    Implemented LoggingAspect using AOP for cleaner, uniform and more manageable logging

--- medical-service/src/main/java/org/health/medical_service/MedicalServiceApplication.java
+++ medical-service/src/main/java/org/health/medical_service/MedicalServiceApplication.java
@@ -2,8 +2,10 @@ package org.health.medical_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class MedicalServiceApplication {
 
 	public static void main(String[] args) {
